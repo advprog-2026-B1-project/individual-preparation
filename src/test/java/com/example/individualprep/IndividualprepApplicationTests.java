af463d1b6331f6dcd71d15bffb42e5de76cf8008
Andrew Wanarahardja <andrew.wanarahardja@gmail.com>
2026-02-12 20:48:32 +0700

    feat/arithmetic-exponent

--- src/test/java/com/example/individualprep/IndividualprepApplicationTests.java
+++ src/test/java/com/example/individualprep/IndividualprepApplicationTests.java
@@ -1,7 +1,10 @@
 package com.example.individualprep;
 
+import com.example.individualprep.service.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class IndividualprepApplicationTests {
@@ -10,4 +13,13 @@ class IndividualprepApplicationTests {
 	void contextLoads() {
 	}
 
+	@Test
+	void testExponentArithmetic() {
+		ArithmeticUtility arithmeticUtility = new ArithmeticUtility();
+		double a = 5;
+		int b = 2;
+
+		double result = arithmeticUtility.exponent(a, b);
+		assertEquals(25, result);
+	}
 }
