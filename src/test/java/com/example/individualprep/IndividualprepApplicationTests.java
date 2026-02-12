95ba55accd4698fb3fc3150117dc6fcafd3e6771
Andrew Wanarahardja <andrew.wanarahardja@gmail.com>
2026-02-12 20:58:14 +0700

    feat/vector-add

--- src/test/java/com/example/individualprep/IndividualprepApplicationTests.java
+++ src/test/java/com/example/individualprep/IndividualprepApplicationTests.java
@@ -22,4 +22,18 @@ class IndividualprepApplicationTests {
 		double result = arithmeticUtility.exponent(a, b);
 		assertEquals(25, result);
 	}
+
+	@Test
+	void testAddVector() {
+		VectorUtility vectorUtility = new VectorUtility();
+		double[] vector1 = {1,2};
+		double[] vector2 = {4,6};
+		double[] vector3 = {7,8,9};
+
+		double[] normalResult = vectorUtility.add(vector1, vector2);
+		double[] abnormalResult = vectorUtility.add(vector2, vector3);
+
+		assertArrayEquals(new double[]{5,8}, normalResult);
+		assertNull(abnormalResult);
+	}
 }
