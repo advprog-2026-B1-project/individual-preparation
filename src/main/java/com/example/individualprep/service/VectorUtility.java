4569e24eeb6bebb22f3819908e5678dfa95a58a9
galihnur9 <galihnurr9@gmail.com>
2026-02-13 12:03:14 +0700

    Implement subtract method in VectorUtility and add corresponding unit test

--- src/main/java/com/example/individualprep/service/VectorUtility.java
+++ src/main/java/com/example/individualprep/service/VectorUtility.java
@@ -19,7 +19,14 @@ public class VectorUtility {
 
     public double[] subtract(double[] v1, double[] v2) {
         // TODO: Implement me properly!
-        return new double[]{0.0, 0.0, 0.0};
+        if (v1.length != v2.length) {
+            return null;
+        }
+        double[] result = new double[v1.length];
+        for (int index = 0; index < v1.length; index++) {
+            result[index] = v1[index] - v2[index];
+        }
+        return result;
     }
 
     public double[] multiply(double[] v1, int x) {
